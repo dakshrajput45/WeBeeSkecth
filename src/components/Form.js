3cf9ac39ec33c73fea25747a04e57662142a4822
dakshrajput45 <dakshr050@gmail.com>
2024-07-06 18:29:46 +0530

    team updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -58,7 +58,7 @@ function Form() {
           </div>
           <div className="relative z-0 w-full mb-5 group">
             <input type="text" name="companyName" id="floating_company" className="block py-3 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-            <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-rgb-0-33-65 peer-focus:dark:text-rgb-0-33-65 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
+            <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-rgb-0-33-65 peer-focus:dark:text-rgb-0-33-65 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company name</label>
           </div>
         </div>
         <div className="relative z-0 w-full mb-5 group">
